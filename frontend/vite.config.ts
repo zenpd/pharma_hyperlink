d9ede63bd4f495b754191ba262aba7bb747689b2
zenpd <zenpd@users.noreply.github.com>
2026-07-02 12:57:32 +0530

    merged with Raja's old code

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5200,
+    port: 5174,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: "http://127.0.0.1:8088",
         changeOrigin: true,
       },
     },
