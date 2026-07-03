afb4bfd6873e12b8a8949b46246e2ce6ecfa05bb
rajazen1 <rajaraman.m@zensar.com>
2026-07-03 13:24:14 +0530

    restyle: bundle fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,8 +6,10 @@ import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
+import "@fontsource/plus-jakarta-sans/500.css";
 import "@fontsource/plus-jakarta-sans/600.css";
 import "@fontsource/plus-jakarta-sans/700.css";
+import "@fontsource/plus-jakarta-sans/800.css";
 
 import App from "./App";
 
